77fc76a83ea225a8263c16de2e8fd06271e0fd6d
Pooja U <pjudawant@gmail.com>
2024-03-13 15:17:40 +0530

    Created project with name BookKart

---
+++ src/main/java/com/example/BookKart/BookKartApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookKart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookKartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookKartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookKart/BookKartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookKart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookKartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
